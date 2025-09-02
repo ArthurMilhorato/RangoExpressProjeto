1f148082c2621f265bfabd39e9885a89d43bb14b
Arthur Ramos Milhorato <158067684+ArthurMilhorato@users.noreply.github.com>
2025-09-02 00:32:53 -0300

    Delete rangodoreicotemig directory

--- rangodoreicotemig/helpers/ConstantsTokens.php
+++
@@ -1,7 +0,0 @@
-<?php
-class ConstantsURLs{
-    public $URL_API_MERCADO_PAGO = "https://api.mercadopago.com";
-    public $URL_API_MERCADO_PAGO_PIX = '/v1/payments';
-    public $API_TOKEN = 'APP_USR-8722381000717359-090117-4b427d4b9a344826d74fc4f9162ba0db-2665646572';
-}   
-?>
